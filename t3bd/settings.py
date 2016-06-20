2fb63a19b24683658a09d08afc79b43ee4e7149e
Paulo Bardes <bardes0022@gmail.com>
2016-06-19 21:53:59 -0300

    Configura o banco para postegrsql

--- t3bd/settings.py
+++ t3bd/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 't3bd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 't3bd',
+        'USER': 'postgres',
+        'HOST': '127.0.0.1'
     }
 }
 
