3a61b26d0ce4fea3bb92c0abca144cbf76a7adb0
Paulo Bardes <bardes0022@gmail.com>
2016-06-21 00:49:03 -0300

    Fix: Diretório de arquivos estáticos

--- t3bd/settings.py
+++ t3bd/settings.py
@@ -73,7 +73,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/s/'
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'paralympics', 'static'),
 ]
