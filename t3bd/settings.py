a7c4f5643124a6db5e5eff53ab09419ae19bbc66
Paulo Bardes <bardes0022@gmail.com>
2016-06-21 01:20:24 -0300

    Fix: ops...

--- t3bd/settings.py
+++ t3bd/settings.py
@@ -58,6 +58,7 @@ WSGI_APPLICATION = 't3bd.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
